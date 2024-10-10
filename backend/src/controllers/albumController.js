bdb21f01c6a127acbbdbe7d974c5049991dd35d2
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-10-09 22:07:44 -0400

    added album controller skeleton

---
+++ backend/src/controllers/albumController.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary'
+import albumModel from '../models/albumModel.js'
+
+const addAlbum = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const listAlbum = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const removeAlbum = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default {addAlbum, listAlbum, removeAlbum}
